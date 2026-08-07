08230c0198d066efacda007276bd3210ab4929f8
DonSimfrontend <128368453+DonSimfrontend@users.noreply.github.com>
2026-08-07 11:56:37 +0000

    fake environment reflection working

--- sketch.js
+++ sketch.js
@@ -16,22 +16,21 @@ function setup() {
   noStroke();
 
   shader(shaderProgram);
+}
+
+function draw() {
+
+  background(0);
 
   shaderProgram.setUniform(
     "u_env",
     envTexture
   );
-}
-
-function draw() {
-  background(0);
 
   shaderProgram.setUniform(
     "u_time",
     millis() * 0.001
   );
-
-  rotateY(millis() * 0.0002);
 
   sphere(
     200,
