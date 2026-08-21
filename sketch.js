ba67a950f44b8dd12fd4bb26a96553c7c1c2824b
DonSimfrontend <128368453+DonSimfrontend@users.noreply.github.com>
2026-08-21 11:17:15 +1000

    Orient texture upright on every cube face

--- sketch.js
+++ sketch.js
@@ -14,7 +14,60 @@ function draw() {
   rotateY(frameCount * 0.015);
 
   texture(textureImage);
-  box(200);
+
+  const s = 100;
+  const u0 = 0;
+  const v0 = 0;
+  const u1 = textureImage.width;
+  const v1 = textureImage.height;
+
+  // Front: image upright
+  beginShape();
+  vertex(-s, -s,  s, u0, v0);
+  vertex( s, -s,  s, u1, v0);
+  vertex( s,  s,  s, u1, v1);
+  vertex(-s,  s,  s, u0, v1);
+  endShape(CLOSE);
+
+  // Back: image upright
+  beginShape();
+  vertex( s, -s, -s, u0, v0);
+  vertex(-s, -s, -s, u1, v0);
+  vertex(-s,  s, -s, u1, v1);
+  vertex( s,  s, -s, u0, v1);
+  endShape(CLOSE);
+
+  // Right: image upright
+  beginShape();
+  vertex( s, -s,  s, u0, v0);
+  vertex( s, -s, -s, u1, v0);
+  vertex( s,  s, -s, u1, v1);
+  vertex( s,  s,  s, u0, v1);
+  endShape(CLOSE);
+
+  // Left: image upright
+  beginShape();
+  vertex(-s, -s, -s, u0, v0);
+  vertex(-s, -s,  s, u1, v0);
+  vertex(-s,  s,  s, u1, v1);
+  vertex(-s,  s, -s, u0, v1);
+  endShape(CLOSE);
+
+  // Top: image upright relative to world Y
+  beginShape();
+  vertex(-s, -s, -s, u0, v0);
+  vertex( s, -s, -s, u1, v0);
+  vertex( s, -s,  s, u1, v1);
+  vertex(-s, -s,  s, u0, v1);
+  endShape(CLOSE);
+
+  // Bottom: image upright relative to world Y
+  beginShape();
+  vertex(-s,  s,  s, u0, v0);
+  vertex( s,  s,  s, u1, v0);
+  vertex( s,  s, -s, u1, v1);
+  vertex(-s,  s, -s, u0, v1);
+  endShape(CLOSE);
 }
 
 function windowResized() {
